b2d7653e20e1567d09fce4714a1e75302e9471ff
Filip Godziński <godzinski.filip@gmail.com>
2017-12-03 12:09:26 +0100

    bild board with tiles

--- src/jsx/main.jsx
+++ src/jsx/main.jsx
@@ -3,8 +3,43 @@ import ReactDOM from 'react-dom';
 
 document.addEventListener('DOMContentLoaded', function() {
 
+    class Tile extends React.Component {
+
+      render() {
+        return <div className='tile' data-index={this.props.num} onClick={this.handleClick} >{this.props.num}</div>
+      }
+    }
+
+    class Board extends React.Component {
+      constructor(props) {
+        super(props);
+        this.state = {
+          tiles: []
+        }
+      }
+
+      renderTile(i) {
+        return <Tile key={i} num={i} onClick={this.clickTile} />
+      }
+
+
+      render() {
+        console.log(this.state.tiles);
+        for(let i=1; i<16; i++) {
+          this.state.tiles.push(this.renderTile(i));
+        }
+
+        this.state.tiles.push(<div className="empty" key={16}></div>)
+
+        return (
+          <div id="game-board">
+              {this.state.tiles}
+          </div>
+        )
+      }
+    }
     ReactDOM.render(
-      <h1>Hello, world</h1>,
+      <Board />,
       document.getElementById('app')
     );
 });
